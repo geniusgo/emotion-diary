cea638ea92a4dbcab5b3cbc22b869d31bc7f2c7d
geniusgo <gogenius28@naver.com>
2024-11-19 15:55:10 +0900

    동적 라우팅 구현 준비 완료

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/new' element={<New />}></Route>
-        <Route path='/diary' element={<Diary />}></Route>
+        <Route path='/diary/:id' element={<Diary />}></Route>
         <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </>
