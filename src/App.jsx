748ff21f7680147d902915c9a9b2a123e0686ce0
geniusgo <gogenius28@naver.com>
2024-11-19 15:40:17 +0900

    Link 컴포넌트와 useNavigate 훅 사용해서 페이지 이동 구현하기

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
 import NotFound from './pages/NotFound';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 /**
  * path 경로 정보
@@ -13,17 +13,31 @@ import { Routes, Route } from 'react-router-dom';
  */
 
 function App() {
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav('/new');
+  };
+
   return (
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/new' element={<New />}></Route>
-      <Route path='/diary' element={<Diary />}></Route>
-      <Route path='*' element={<NotFound />}></Route>
-    </Routes>
+    <>
+      <div>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/new'}>new</Link>
+        <Link to={'/diary'}>diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/new' element={<New />}></Route>
+        <Route path='/diary' element={<Diary />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
+    </>
   );
 }
 
 // 잘못된 경로로 들어오면 path *로 설정하고 element에 NotFound 전달
-// Routes 컴포넌트 안에는 Roㅕㅅㄷ
+// Routes 컴포넌트 안에는 Route 컴포넌트만 사용 가능하다
+// Routes 컴포넌트 밖에서 element를 추가하면 모든 페이지에서 보인다
 
 export default App;
