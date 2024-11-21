fd389c2021ca64a25e5b5a7abfa78a6e2ee49e7b
geniusgo <gogenius28@naver.com>
2024-11-21 22:20:53 +0900

    date-formater 날짜 표시 방식 수정

--- emotion-diary/src/pages/Home.jsx
+++ emotion-diary/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import Header from '../compoenets/Header';
 import Button from '../compoenets/Button';
 import DiaryList from '../compoenets/DiaryList';
+import { useContext } from 'react';
+import { DiaryStateContext } from '../App';
 
 const Home = () => {
+  const diary = useContext(DiaryStateContext);
+
   // 누르면 나오는 목록 날짜 기간에 맞게 바뀌도록 변경 필요
   const onClick = (e) => {
     console.log(e.target);
--- emotion-diary/src/utils/date-formater.js
+++ emotion-diary/src/utils/date-formater.js
@@ -1,18 +1,18 @@
 export const dateFormater = (timeFormatDate) => {
-  let year = new Date(timeFormatDate).getFullYear();
-  let month = new Date(timeFormatDate).getMonth();
-  let date = new Date(timeFormatDate).getDate();
-
-  console.log(new Date(timeFormatDate), timeFormatDate);
+  console.log(new Date(timeFormatDate));
+  let year = new Date(timeFormatDate).getFullYear().toString();
+  let month = (new Date(timeFormatDate).getMonth() + 1).toString();
+  let date = new Date(timeFormatDate).getDate().toString();
 
   console.log(year, month, date);
 
-  // if (month.length < 2) {
-  //   month = '0' + month;
-  // }
+  if (month.length < 2) {
+    month = '0' + month;
+  }
 
-  // if (date.length < 2) {
-  //   date = '0' + date;
-  // }
+  if (date.length < 2) {
+    date = '0' + date;
+  }
+
   return `${year}. ${month}. ${date}`;
 };
