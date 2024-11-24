e96130aeb65b7bbeac8e0ea0910d4ef14146fd5c
geniusgo <gogenius28@naver.com>
2024-11-24 21:17:56 +0900

    새 일기 추가하기 기능

--- emotion-diary/src/components/Footer.jsx
+++ emotion-diary/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import './Footer.css';
 import Button from './commons/Button';
 
-const Footer = ({ onClick, onCreate, onUpdate }) => {
+const Footer = ({ type, onClick, onCreate, onUpdate }) => {
   // 이벤트 핸들러 이름 중복 이슈 해결이 필요
   return (
     <div className='footer-container'>
       <Button onClick={onClick} text='취소하기' />
-      <Button onClick={onCreate || onUpdate} text='작성 완료' type='positive' />
+      <Button onClick={type === 'edit' ? onUpdate : onCreate} text='작성 완료' type='positive' />
     </div>
   );
 };
--- emotion-diary/src/pages/Edit.jsx
+++ emotion-diary/src/pages/Edit.jsx
@@ -15,6 +15,7 @@ const Edit = () => {
   const [diaryDate, setDiaryDate] = useState(selectedDiary.diaryDate);
   const [emotionId, setEmotionId] = useState(selectedDiary.emotionId); // emotionId 클릭된 값 저장
   const [content, setContent] = useState(selectedDiary.content);
+  const type = params.id ? 'edit' : 'new';
 
   const handlePageMoveToHome = () => {
     if (window.confirm('변경 사항이 저장되지 않습니다. 계속 할까요?')) {
@@ -29,13 +30,6 @@ const Edit = () => {
     }
   };
 
-  const handleDiaryDelete = () => {
-    if (window.confirm('삭제된 일기는 복구되지 않습니다. 계속 할까요?')) {
-      handleDelete(selectedDiary.id);
-      nav('/', { replace: true });
-    }
-  };
-
   const handleDiaryUpdate = () => {
     if (window.confirm('작성한 내용으로 수정할까요?')) {
       handleUpdate({ id: selectedDiary.id, diaryDate, emotionId, content });
@@ -43,16 +37,23 @@ const Edit = () => {
     }
   };
 
+  const handleDiaryDelete = () => {
+    if (window.confirm('삭제된 일기는 복구되지 않습니다. 계속 할까요?')) {
+      handleDelete(selectedDiary.id);
+      nav('/', { replace: true });
+    }
+  };
+
   return (
     <div>
       <Header
-        type={params.id ? 'edit' : 'new'}
+        type={type}
         text='일기 수정하기'
         leftBtn={<Button text='< 뒤로 가기' onClick={handlePageMoveToHome} />}
         rightBtn={<Button text='삭제하기' type='negative' onClick={handleDiaryDelete} />}
       />
       <Editor
-        type={params.id ? 'edit' : 'new'}
+        type={type}
         diaryDate={diaryDate}
         emotionId={emotionId}
         content={content}
@@ -60,7 +61,12 @@ const Edit = () => {
         setEmotionId={setEmotionId}
         setContent={setContent}
       />
-      <Footer onClick={handlePageMoveToHome} onUpdate={handleDiaryUpdate}></Footer>
+      <Footer
+        type={type}
+        onClick={handlePageMoveToHome}
+        onCreate={handleNewDiaryCreate}
+        onUpdate={handleDiaryUpdate}
+      ></Footer>
     </div>
   );
 };
