7f5249a373b6f262500cee98fe8538c9a21f89cf
geniusgo <gogenius28@naver.com>
2024-11-24 21:14:30 +0900

    new 페이지 일기 쓰기 기본 페이지 준비 완료

--- emotion-diary/src/components/commons/EmotionCard.jsx
+++ emotion-diary/src/components/commons/EmotionCard.jsx
@@ -1,8 +1,7 @@
 import './EmotionCard.css';
 import { emotionName } from '../../constants/constants';
 
-// details에도 쓸 수 있을 것 같고(사이즈 조절해서), edit/new 다 사용 가능
-const EmotionCard = ({ emotionId, selectedEmotionId = emotionId }) => {
+const EmotionCard = ({ emotionId, selectedEmotionId = 0 }) => {
   return (
     <div className={`emotion-card emotion${selectedEmotionId === emotionId ? emotionId : '0'}`}>
       <img src={`./../src/assets/emotion${emotionId}.png`} alt='emotion-image' />
